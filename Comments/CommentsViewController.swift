375a5f16f2bdcbf768a9d813ae0c1d5fe64546df
farhad519 <farhadchowdhury519@gmail.com>
2022-08-18 00:35:40 +0600

    Implement comments view

---
+++ Comments/CommentsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CommentsViewController.swift
+//  AuctionHouse
+//
+//  Created by Farhad Chowdhury on 15/8/22.
+//
+
+import UIKit
+
+class CommentsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
